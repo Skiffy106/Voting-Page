c520e0d28f3365b70e7a5c57410cdc1d8cc0379c
Mason Tuttle <76038336+Skiffy106@users.noreply.github.com>
2024-02-02 03:55:23 -0800

    Added Json Support

--- app.js
+++ app.js
@@ -10,65 +10,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// Read the candidates JSON file
+const candidatesData = JSON.parse(fs.readFileSync("candidates.json", "utf-8"));
+
 // Routes
 app.get("/", (req, res) => {
   // Render your updated voting page here (e.g., index.ejs)
-  res.render("index");
+  res.render("index", { candidatesData });
 });
 
 app.post("/submit-vote", (req, res) => {
-  //   console.log(req);
-  // Extract data from the form submission
-  const presidentData = {
-    selectedCandidate: req.body.presidentCandidate,
-    category: "president",
-  };
+  let csvData = "";
+  const categoryArray = req.body.category;
 
-  const vicePresidentData = {
-    selectedCandidate: req.body.vicePresidentCandidate,
-    category: "vice-president",
-  };
-
-  const treasurerData = {
-    selectedCandidate: req.body.treasurerCandidate,
-    category: "treasurer",
-  };
-
-  const secretaryData = {
-    selectedCandidate: req.body.secretaryCandidate,
-    category: "secretary",
-  };
-
-  const promoterData = {
-    selectedCandidate: req.body.promoterCandidate,
-    category: "promoter",
-  };
-
-  const projectManagerData = {
-    selectedCandidate: req.body.projectManagerCandidate,
-    category: "project-manager",
-  };
-
-  //   console.log(presidentData);
-  //   console.log(vicePresidentData);
-  // Create CSV format strings for each category
-  const presidentCsvData = `${presidentData.category},${presidentData.selectedCandidate}\n`;
-  const vicePresidentCsvData = `${vicePresidentData.category},${vicePresidentData.selectedCandidate}\n`;
-  const treasurerCsvData = `${treasurerData.category},${treasurerData.selectedCandidate}\n`;
-  const secretaryCsvData = `${secretaryData.category},${secretaryData.selectedCandidate}\n`;
-  const promoterCsvData = `${promoterData.category},${promoterData.selectedCandidate}\n`;
-  const projectManagerCsvData = `${projectManagerData.category},${projectManagerData.selectedCandidate}\n`;
-
-  const data =
-    presidentCsvData +
-    vicePresidentCsvData +
-    treasurerCsvData +
-    secretaryCsvData +
-    promoterCsvData +
-    projectManagerCsvData;
+  for (let index = 0; index < categoryArray.length; index++) {
+    let candidate = req.body[`${categoryArray[index]}Candidate`];
+    csvData += `${categoryArray[index]},${candidate}\n`;
+  }
 
   // Append the data to the CSV file
-  fs.appendFile("votes.csv", data, (err) => {
+  fs.appendFile("votes.csv", csvData, (err) => {
     if (err) {
       console.error(err);
       res.status(500).send("Error submitting votes");
